8ef184a8c997928c23edc5bc3a828978045b5cc8
David Nix <hello@davidnix.io>
2023-09-07 17:08:20 -0600

    Add mailbox stubs

--- rust/chains/hyperlane-cosmos/src/mailbox.rs
+++ rust/chains/hyperlane-cosmos/src/mailbox.rs
@@ -1,13 +1,100 @@
-use std::fmt::Debug;
+use std::fmt::{Debug, Formatter};
+use std::num::NonZeroU64;
 use std::ops::RangeInclusive;
 use std::sync::Arc;
 use std::time::Duration;
+use tracing::instrument;
 use url::Url;
 
 use async_trait::async_trait;
 use eyre::Result;
-use hyperlane_core::{ChainCommunicationError, ChainResult, HyperlaneMessage, Indexer, LogMeta};
+use hyperlane_core::{
+    accumulator::incremental::IncrementalMerkle, utils::fmt_bytes, ChainCommunicationError,
+    ChainResult, Checkpoint, HyperlaneChain, HyperlaneContract, HyperlaneDomain, HyperlaneMessage,
+    HyperlaneProvider, Indexer, LogMeta, Mailbox, TxCostEstimate, TxOutcome, H256, U256,
+};
 use tendermint_rpc::client::Client;
+
+/// A reference to a Mailbox contract on some Cosmos chain
+pub struct CosmosMailbox {}
+
+impl HyperlaneContract for CosmosMailbox {
+    fn address(&self) -> H256 {
+        todo!()
+    }
+}
+
+impl HyperlaneChain for CosmosMailbox {
+    fn domain(&self) -> &HyperlaneDomain {
+        todo!()
+    }
+
+    fn provider(&self) -> Box<dyn HyperlaneProvider> {
+        todo!()
+    }
+}
+
+impl Debug for CosmosMailbox {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self as &dyn HyperlaneContract)
+    }
+}
+
+#[async_trait]
+impl Mailbox for CosmosMailbox {
+    #[instrument(level = "debug", err, ret, skip(self))]
+    async fn tree(&self, lag: Option<NonZeroU64>) -> ChainResult<IncrementalMerkle> {
+        todo!()
+    }
+
+    #[instrument(level = "debug", err, ret, skip(self))]
+    async fn count(&self, lag: Option<NonZeroU64>) -> ChainResult<u32> {
+        todo!()
+    }
+
+    #[instrument(level = "debug", err, ret, skip(self))]
+    async fn delivered(&self, id: H256) -> ChainResult<bool> {
+        todo!()
+    }
+
+    #[instrument(level = "debug", err, ret, skip(self))]
+    async fn latest_checkpoint(&self, lag: Option<NonZeroU64>) -> ChainResult<Checkpoint> {
+        todo!()
+    }
+
+    #[instrument(err, ret, skip(self))]
+    async fn default_ism(&self) -> ChainResult<H256> {
+        todo!()
+    }
+
+    #[instrument(err, ret, skip(self))]
+    async fn recipient_ism(&self, recipient: H256) -> ChainResult<H256> {
+        todo!()
+    }
+
+    #[instrument(err, ret, skip(self))]
+    async fn process(
+        &self,
+        message: &HyperlaneMessage,
+        metadata: &[u8],
+        tx_gas_limit: Option<U256>,
+    ) -> ChainResult<TxOutcome> {
+        todo!()
+    }
+
+    #[instrument(err, ret, skip(self), fields(msg=%message, metadata=%fmt_bytes(metadata)))]
+    async fn process_estimate_costs(
+        &self,
+        message: &HyperlaneMessage,
+        metadata: &[u8],
+    ) -> ChainResult<TxCostEstimate> {
+        todo!()
+    }
+
+    fn process_calldata(&self, message: &HyperlaneMessage, metadata: &[u8]) -> Vec<u8> {
+        todo!()
+    }
+}
 
 /// Retrieves event data for a Cosmos chain that uses the hyperlane modules.
 #[derive(Debug)]
