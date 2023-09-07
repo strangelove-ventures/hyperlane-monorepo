4f6a92efac8f10d40ab929216859da007aa4d19e
David Nix <hello@davidnix.io>
2023-09-07 17:09:55 -0600

    Add multisig_ism stub

--- rust/chains/hyperlane-cosmos/src/lib.rs
+++ rust/chains/hyperlane-cosmos/src/lib.rs
@@ -5,3 +5,4 @@
 #![allow(unused)]
 
 pub mod mailbox;
+pub mod multisig_ism;
---
+++ rust/chains/hyperlane-cosmos/src/multisig_ism.rs
@@ -0,0 +1,36 @@
+use async_trait::async_trait;
+use hyperlane_core::{
+    ChainResult, HyperlaneChain, HyperlaneContract, HyperlaneDomain, HyperlaneMessage,
+    HyperlaneProvider, MultisigIsm, H256,
+};
+
+/// A reference to a MultisigIsm contract on some Cosmos chain
+#[derive(Debug)]
+pub struct CosmosMultisigIsm {}
+
+impl HyperlaneContract for CosmosMultisigIsm {
+    fn address(&self) -> H256 {
+        todo!()
+    }
+}
+
+impl HyperlaneChain for CosmosMultisigIsm {
+    fn domain(&self) -> &HyperlaneDomain {
+        todo!()
+    }
+
+    fn provider(&self) -> Box<dyn HyperlaneProvider> {
+        todo!()
+    }
+}
+
+#[async_trait]
+impl MultisigIsm for CosmosMultisigIsm {
+    /// Returns the validator and threshold needed to verify message
+    async fn validators_and_threshold(
+        &self,
+        message: &HyperlaneMessage,
+    ) -> ChainResult<(Vec<H256>, u8)> {
+        todo!()
+    }
+}
